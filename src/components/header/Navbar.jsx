fd33cf7fb4ce6885edcc473cd03b5585602f4563
cherryc0ck <qstar9537@naver.com>
2023-03-16 21:40:21 +0900

    feat: 내비게이션바 구현

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
-import Firebase from "service/Firebase";
+import User from "components/user/User";
+import Button from "components/ui/Button";
+import { useAuthContext } from "context/AuthContext";
 
 const Navbar = () => {
+  const { user, login, logout } = useAuthContext();
+
   return (
     <StyledHeader>
       <StyledLink to="/">
@@ -14,11 +18,18 @@ const Navbar = () => {
       </StyledLink>
       <StyledNavbar>
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
-        <Link to="/products/new">
-          <BsFillPencilFill />
-        </Link>
-        <Firebase />
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {user && <User user={user} />}
+        {user ? (
+          <Button text="Logout" onClick={logout} />
+        ) : (
+          <Button text="login" onClick={login} />
+        )}
       </StyledNavbar>
     </StyledHeader>
   );
@@ -45,7 +56,8 @@ const StyledNavbar = styled.nav`
     align-items: center;
     gap: 1rem;
 
-    a {
+    a,
+    button {
       color: ${theme.colors.gray600};
       ${theme.typo.medium16};
     }
