8abd9bb3d1a830a5cf67630885a915b4600885ea
cherryc0ck <qstar9537@naver.com>
2023-03-18 15:40:33 +0900

    Style: 코드포맷팅 수정

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -3,10 +3,8 @@ import React from "react";
 import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children, requireAdmin }) => {
-  console.log(requireAdmin);
   const { user } = useAuthContext();
   if (!user || (requireAdmin && !user.isAdmin)) {
-    console.log("ddd");
     return <Navigate to="/" replace />;
   }
 
